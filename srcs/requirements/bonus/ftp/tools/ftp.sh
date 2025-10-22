4dea320f8457374b942d3ff81e6eb33634329292
Yassir Ait Ammi <yassiraitammi@gmail.com>
2025-10-22 15:30:40 +0100

    Done::

---
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /var/run/vsftpd/empty
+mkdir -p /var/www/wordpress
+
+if ! id -u $FTP_USER > /dev/null 2>&1; then
+    useradd -m -d /var/www/wordpress $FTP_USER
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+fi
+
+chown -R $FTP_USER:$FTP_USER /var/www/wordpress
+
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
---
+++ srcs/requirements/wordpress/conf/redis.php
@@ -0,0 +1,5 @@
+<?php
+define( 'WP_CACHE', true );
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_SCHEME', 'tcp' );
